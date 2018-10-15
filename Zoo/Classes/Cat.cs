e56e06ff1ee32fac9f56871276b9aca049d79520
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 10:29:51 -0700

    Add two virtual properties in Cat class

--- Zoo/Classes/Cat.cs
+++ Zoo/Classes/Cat.cs
@@ -4,8 +4,27 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    public class Cat
+    public abstract class Cat
     {
+        protected abstract bool Limber { get; set; }
+        protected virtual bool MarvelousHearing { get; set; }
+        protected virtual string Ears { get; set; }
+
+        protected virtual string Sleeps()
+        {
+            return "Cats usually sleep during the day. They are nocturnal.";
+        }
+
+        protected virtual string Plays()
+        {
+            return "Likes to play in boxes.";
+        }
+
+        protected virtual string Moves()
+        {
+            return "They are graceful animals. They move like ballerinas.";
+        }
+
         public Cat()
         {
         }
