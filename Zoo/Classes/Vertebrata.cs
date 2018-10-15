5acb879529e132676c8bab03e237fcb4fd945880
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 09:55:22 -0700

    Add namespaces

--- Zoo/Classes/Vertebrata.cs
+++ Zoo/Classes/Vertebrata.cs
@@ -1,3 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+
+namespace Zoo.Classes
+{
+    public class Vertebrata
+    {
+        public Vertebrata()
+        {
+        }
+    }
+}
