14e9388037aae4d8368e91d8b3018c54d4d27c71
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 20:31:14 -0700

    Add to SoftShellTurtle concrete class

--- Zoo/Classes/SoftShellTurtle.cs
+++ Zoo/Classes/SoftShellTurtle.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    public class SoftShellTurtle
+    public class SoftShellTurtle : Turtle
     {
-        public SoftShellTurtle()
+        public string Name { get; set; }
+        public override string MovingSpeed { get => "Faster than other land turtles"; }
+
+        public override string LikesCats()
         {
+            return "Isn't fond of cats because they have dangerous nails. We're soft shelled.";
+        }
+
+        public SoftShellTurtle(string name)
+        {
+            Name = name;
         }
     }
 }
