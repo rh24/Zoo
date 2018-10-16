e078c00fe62f4dcd85f4a56200695fb65992fdef
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 20:50:29 -0700

    Add two passing tests for SoftShellTurtle concrete class

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,15 +1,25 @@
 using System;
 using Xunit;
+using Zoo.Classes;
 
 namespace ZooTests
 {
     public class ZooTests
     {
+
+        SoftShellTurtle softie = new SoftShellTurtle("Softie");
+
         // Test that all concrete classes inherit properties Endoskeleton, Brain
         [Fact]
-        public void Test1()
+        public void InheritsVertebrataProperty1()
         {
+            Assert.IsType<string>(softie.Endoskeleton);
+        }
 
+        [Fact]
+        public void InheritsVertebrataProperty2()
+        {
+            Assert.IsType<string>(softie.Brain);
         }
     }
 }
