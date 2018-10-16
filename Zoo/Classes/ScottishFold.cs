309ec036fe0de76a27ec4efe0491281e18fe636c
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 22:54:50 -0700

    Refactor to use expression bodied members and implement IApproachable in ScottishFold concrete class

--- Zoo/Classes/ScottishFold.cs
+++ Zoo/Classes/ScottishFold.cs
@@ -7,31 +7,36 @@ namespace Zoo.Classes
 {
     public class ScottishFold : Cat, IApproachable
     {
+        // Propeties from pt.1 of OOP Zoo lab
         public override string Cuteness { get; set; } = "Overload";
         public override bool Limber { get => false; }
         public override string Name { get; set; }
         public override string Ears { get => "Folded ears"; }
         public override bool MarvelousHearing { get; set; } = false;
 
+        // Properties from pt.2 IApproachable implementation
+        public bool ReceivedShots { get; set; } = true;
+        public bool WillBite { get; set; } = false;
+        public bool MightCharge { get; set; } = false;
+        public bool LikesAttention { get; set; } = true;
+
+        // Methods from pt.1
         public ScottishFold(string name) : base(name)
         {
             Name = name;
         }
 
-        public override string Sleeps()
+        public override string Sleeps() => "But this cat sleeps at night with its hooman.";
+
+        public override string Moves() => $"{base.Moves()} However, this cat has a hereditary cartilage disease that makes moving painful.";
+
+        public override string GetDemeanor() => $"Lazy, affectionate, and, sometimes, {base.GetDemeanor().ToLower()}.";
+
+        // Method from pt.2 IApproachable implementation
+        public string ShouldTouristApproach()
         {
-            return "But this cat sleeps at night with its hooman.";
+            return "Yes";
         }
 
-        public override string Moves()
-        {
-            return $"{base.Moves()} However, this cat has a hereditary cartilage disease that makes moving painful.";
-        }
-
-        // test to prove inheritance => "Lazy, affectionate, and, sometimes, playful."
-        public override string GetDemeanor()
-        {
-            return $"Lazy, affectionate, and, sometimes, {base.GetDemeanor().ToLower()}.";
-        }
     }
 }
