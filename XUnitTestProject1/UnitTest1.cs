123999b26d4d5cfff72e253afa67e220ac0cff20
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 21:10:04 -0700

    Add ScottishFold passing tests

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -50,5 +50,29 @@ namespace ZooTests
             string checkAgainst = mainie.MakeSound();
             Assert.Equal(expected, checkAgainst);
         }
+
+        ScottishFold waffles = new ScottishFold("Waffles");
+
+        /// <summary>
+        /// Test that ScottishFold inherits "Playful" from base class Cat when checking its GetDemeanor() method.
+        /// </summary>
+        [Fact]
+        public void CheckScottishFoldInheritsPlayful()
+        {
+            string playful = mainie.GetDemeanor();
+            bool isContained = waffles.GetDemeanor().Contains(playful);
+            Assert.True(isContained);
+        }
+
+        /// <summary>
+        /// Test that ScottishFold inherits part of Moves() from base Cat class. Moves() in a non-overriden derivative object would return "Normally, cats are graceful animals. They move like ballerinas." This method will test whether an object of type Scottish fold contains this string. Even though I've overriden the base behvior in my ScottishFold class, I still use the base.Moves() return value within my newly returned string.
+        /// </summary>
+        [Fact]
+        public void CheckScottishFoldMoves()
+        {
+            string baseString = mainie.Moves();
+            bool isContained = waffles.Moves().Contains(baseString);
+            Assert.True(isContained);
+        }
     }
 }
