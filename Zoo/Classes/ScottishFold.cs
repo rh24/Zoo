7e131c84f837023c5b24521560e047113d53ae21
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 18:53:53 -0700

    Override abstract property in Scottish Fold class

--- Zoo/Classes/ScottishFold.cs
+++ Zoo/Classes/ScottishFold.cs
@@ -6,7 +6,7 @@ namespace Zoo.Classes
 {
     public class ScottishFold : Cat
     {
-        public string Cuteness { get; } = "Overload";
+        public override string Cuteness { get; } = "Overload";
         public override bool Limber { get => false; }
         public string Name { get; set; }
         public override string Energy { get; set; } = "Lazy";
