6e10692629d86d99b25beeb38a811d68c3705a96
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 19:43:55 -0700

    Add to Rodent class

--- Zoo/Classes/Rodent.cs
+++ Zoo/Classes/Rodent.cs
@@ -4,10 +4,18 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    public class Rodent
+    public class Rodent : Mammal
     {
-        public Rodent()
+        public override int AvgLifeSpanInYears { get; set; } = 5;
+
+        public override string MakeSound()
         {
+            return "Squeak";
+        }
+
+        public virtual string GrowsUntil()
+        {
+            return "Size of a tennis ball";
         }
     }
 }
