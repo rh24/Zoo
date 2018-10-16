bacc43a4ef637b2e148c224a5890ca5bd7a0c196
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 18:26:58 -0700

    Add sphynx and softshell turtle classes

---
+++ Zoo/Classes/SoftShellTurtle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    public class SoftShellTurtle
+    {
+        public SoftShellTurtle()
+        {
+        }
+    }
+}
