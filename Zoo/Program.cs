c406501364801f42fb3274cf151439654f642b51
rebeccahong <rebeccayhong@gmail.com>
2018-10-15 20:34:34 -0700

    Check that softshellturtle overrides inherited property with expression bodied member

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -10,6 +10,12 @@ namespace Zoo
             ScottishFold waffles = new ScottishFold("Waffles");
             Console.WriteLine($"Traits of this {waffles.GetType()}:" + Environment.NewLine + $"{waffles.Cuteness}");
             Console.WriteLine(waffles.Moves());
+
+            SoftShellTurtle softie = new SoftShellTurtle("Softie");
+            Console.WriteLine(softie.MovingSpeed);
+            // hopefully, this has been overriden
+            // Turtle.MovingSpeed == "slow"
+            // softie.MovingSpeed == "Faster than other land turtles"
         }
     }
 }
